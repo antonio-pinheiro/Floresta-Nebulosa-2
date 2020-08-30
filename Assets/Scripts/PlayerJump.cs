da1522fe1b78f3316bf926b2500e1278d69d086c
antonio-pinheiro <antonio.fatecjd@gmail.com>
2020-08-30 20:21:51 -0300

    Primeiro commit

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+    Animator anim;
+
+    public float intervaloDePulo;
+    private float proximoPulo;
+
+    public AudioClip jumpSound;
+    private AudioSource audioS;
+	// Use this for initialization
+	void Start () {
+
+        anim = gameObject.GetComponent<Animator>();
+        audioS = gameObject.GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown("space") && Time.time > proximoPulo)
+        {
+            Pulou();
+        }
+
+	}
+
+    void Pulou()
+    {
+        audioS.clip = jumpSound;
+        audioS.Play();
+        anim.SetTrigger("Pulou");
+        proximoPulo = Time.time + intervaloDePulo;
+    }
+}
